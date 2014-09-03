12f2f7a4b0aec05550f13d29dd688646699aca86
Tom Blackmore <tablackmore@gmail.com>
2014-09-03 20:05:43 +0200

    removed browser launcher

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-/************************************************************************
- *  Copyright 2010-2011 Worlize Inc.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- ***********************************************************************/
-
 var WebSocketServer = require('websocket').server;
 var express = require('express');
 var http = require('http');
 var app = express();
-var launcher = require('browser-launcher');
 
 app.use(express.static(__dirname + "/public"));
 app.server = http.createServer(app);
 app.server.listen(8080);
-
 
 var wsServer = new WebSocketServer({
     httpServer: app.server,
@@ -76,16 +58,3 @@ wsServer.on('request', function (request) {
 });
 
 console.log("Textarea test app ready");
-launcher(function (err, launch) {
-    if (err) return console.error(err);
-    /*
-    console.log('# available browsers:');
-    console.dir(launch.browsers);
-    */
-    launch('http://localhost:8080', {
-        browser: 'chrome'
-    }, function (err, ps) {
-        if (err) return console.error(err);
-    });
-
-});
